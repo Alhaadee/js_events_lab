9ae82ead279c5a171a56420933d72b9d230f2827
Alhaadee <alhaadee91@gmail.com>
2022-09-27 15:27:14 +0100

    added completed list

--- script.js
+++ script.js
@@ -3,16 +3,39 @@ const button = document.getElementById("enter");
 button.addEventListener("click",()=>{
     const input = document.getElementById("new-todo").value 
     const listItem = document.createElement("li")
+    const completedList = document.getElementById("completed_list")
     listItem.innerText = input;
 
     const deleteBtn = document.createElement("button")
     deleteBtn.innerText ="Delete"
 
+    const checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+
     listItem.appendChild(deleteBtn);
+    listItem.appendChild(checkBox);
     list.appendChild(listItem)
     
         deleteBtn.addEventListener("click",()=>{
             list.removeChild(listItem);
+        })
+
+    const completedItem = document.createElement("li")
+    completedItem.innerText = input;
+    const deleteCompletedBtn = document.createElement("button")
+    deleteCompletedBtn.innerText ="Delete"
+    completedItem.appendChild(deleteCompletedBtn)
+
+        deleteCompletedBtn.addEventListener("click",()=>{
+            completedList.removeChild(completedItem);
+        })
+
+    
+        checkBox.addEventListener("click",()=>{
+            
+            listItem.style.backgroundColor = "#d9dad7";
+            completedItem.style.backgroundColor = "white";
+            completedList.appendChild(completedItem)
         })
 })
 
@@ -25,4 +48,6 @@ dateButton.addEventListener("click", () => {
     date.innerText = Date();
     showDate.appendChild(date);
 });
+
+
 
