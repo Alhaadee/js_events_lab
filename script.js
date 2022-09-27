304a91d4abb30ff2cf6cb48ed57c773e18fd56e6
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-27 16:21:17 +0300

    added delete item feature

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ button.addEventListener("click",()=>{
     const input = document.getElementById("new-todo").value 
     const listItem = document.createElement("li")
     listItem.innerText = input;
+
     const deleteBtn = document.createElement("button")
-    deleteBtn.className = "delete"
     deleteBtn.innerText ="Delete"
+    
     listItem.appendChild(deleteBtn);
-    const list = document.querySelector("#list");
     list.appendChild(listItem)
+    
+        deleteBtn.addEventListener("click",()=>{
+            list.removeChild(listItem);
+        })
 })
 
-
-deleteBtn.addEventListener("click",()=>{
-    deleteBtn.
-    list.
-    // 
-})
